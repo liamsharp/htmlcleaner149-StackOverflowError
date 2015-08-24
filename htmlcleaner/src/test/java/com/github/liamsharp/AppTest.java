eb911dacd74f19975ca9bbd263c2a966d3d8301f
Liam Sharp <liam.sharp@gmail.com>
2015-08-24 09:24:28 +0100

    Code to reproduce bug as a test case.

--- htmlcleaner/src/test/java/com/github/liamsharp/AppTest.java
+++ htmlcleaner/src/test/java/com/github/liamsharp/AppTest.java
@@ -1,5 +1,12 @@
 package com.github.liamsharp;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -29,10 +36,27 @@ public class AppTest
     }
 
     /**
-     * Rigourous Test :-)
+     * Test to expose https://sourceforge.net/p/htmlcleaner/bugs/149/
      */
-    public void testApp()
+    public void testStackOverflowError()
     {
-        assertTrue( true );
+        final String filePath = System.getProperty("user.dir") + System.getProperty("file.separator") + "src"
+                + System.getProperty("file.separator") + "test/resources/sof.html";
+        try
+        {
+            System.out.println("filePath: " + filePath);
+            final String html = FileUtils.readFileToString(new File(filePath));
+            new HtmlCleaner().clean(html);
+        }
+        catch (StackOverflowError e)
+        {
+            System.err.println("StackOverflowError: " + e);
+            fail("Stack overflowed!");
+        }
+        catch (IOException e)
+        {
+            System.err.println("Unable to load file: " + filePath + ", " + e);
+            fail("Couldn't find file!");
+        }
     }
 }
